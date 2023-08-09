ed06ef43012f8a8f0dc62c6bac6e40d58d46da63
vie_bcit <vienna_ly@bcit.ca>
2023-08-08 17:42:03 -0700

    v2 multicolumn updates

--- readme.md
+++ readme.md
@@ -1,14 +1,54 @@
 # About
 
+The scripts create `Select` components to replace table column content.  
+`dropdownMatching` is an older version and processes only 2-columns; `ddm_Columns` is the updated version for multi-column support. 
+
+## dropdownMatching - legacy -
+
+`scripts/dropdownMatching`  
+Creates drop down using Select component to match Column2 content to Column1 content.
 Updated from _SOH CARD 4201 activity (archived from test.ltc.bcit).  Modified to create the data from table in the body.
 
-## Set up activity from template
+**HTML content requirements**: 2 columns table.  
 
-The `sampleTemplate.html` provides the table structure needed.  
-Copy the template html and replace table content.
-First column are the questions, second column are the answers.
+- First column are the questions
+- Second column are the answers, the content of which will be the options for Select component. 
 Selection component options will be from all the answers (duplicates will be combined so only unique choices)
 
-## Source files
+## ddm_Columns
 
-scripts and resources published to BCIT Learning Hub shared.
+`scripts/ddm_Columns`  
+This is an updated Drop down creator that allows for multi-column tables.  
+
+### HTML
+
+Identify the columns to keep change by adding `_ddm` class to the `th`. Static columns keep original content. Corresponding columns `td` content will be replaced with `select` components with aggregated unique options based on the texts in the column.
+
+If no `th` marked with class `_ddm`, then the last column will be converted.
+
+**HTML content requirements**: a table with >1 columns, e.g.
+
+```html
+<table class="compact striped left">
+      <thead>
+        <tr>
+          <th class="_ddm" width="20%">Drug</th>
+          <th>Indications</th>
+          <th >Action</th>
+          <th class="_ddm" width="20%">Key Points of Administration &amp; Mode of Administration</th>
+          <th>Considerations</th>
+          <th>Adverse effects/ Side effects</th>     
+        </tr>
+```
+
+### styles
+
+`ddm_Columns.css` contains minimal styles for functionality so it can inherit other global styles (`bcit.css`, `boostrap` ..etc), for consistency with other components.
+
+#### optional styles
+
+- `table class="left"` to text align content left (over ride `bcit.css` table formatting).
+
+## Deployed source files
+
+Scripts published to BCIT Learning Hub shared folder: `https://learn.bcit.ca/shared/scripts/interactive/tableFill_DropDownSelect/`.
---
+++ scripts/ddm_v2.js
@@ -0,0 +1,141 @@
+/**Reads content from table and creates a select dropdown activity
+ * - Unique select options are created per column `<td>`, excluding class="_static"
+ * - `<th>` and `<td>` of class="_static" are shown
+ */
+$(document).ready(function() {
+	
+	
+	CreateDDMatching();
+
+	/** Set up UI elems for dropdown matching activity 
+	 * The container elem is #ddMatching
+	*/
+	function CreateDDMatching() {
+		const $div = $("<div id='ddMatching'></div>");
+		$("body").append($div);			
+
+		$div.append($("table"));
+
+		// buttons
+		const $uibtns = UI_createBns();
+		$div.append($uibtns);
+
+		UI_CreateSelects();			
+	}
+
+	/** @returns $container div with UI buttons */
+	function UI_createBns() {
+		const $container = $("<div class='ui-btn-container'></div>");
+		const $btn_checkAns = $("<button id='check-ans' class='btn btn-info' type='button'>Check Answers</button>");
+		const $btn_reset = $("<button  id='reset-ans' class='btn btn-info' type='button'>Clear</button>");
+
+		// bind events
+		$btn_checkAns.click(function(){CheckAnswers()});
+		$btn_reset.click(function(){ResetAnswers()})
+		
+		$container.append($btn_checkAns, $btn_reset);
+		return $container;
+	}
+
+	/** Replaces td content with dropdown select and icon,  
+	 * if corresponding th not '._static' class
+	*/
+	function UI_CreateSelects() {
+		// get th not _static class
+		let $ths = $("th._ddm");
+
+		if ($ths.length==0) {
+			//console.log($ths);
+			//"if no th marked as class _static, then replace only the last column"
+			$ths = $("th:last");
+		}
+
+		$ths.each( (n,th) => {	
+			
+			const colindex = $(th).index()+1;
+			const $colntds = $("table tr td:nth-child(" + colindex + ")");
+			let selOptions = [];
+
+			$colntds.each((i,td) => {
+				selOptions.push($(td).text().replace(/\n */g, "").trim());
+			});
+			
+			// order, only unique values
+			//selOptions = Randomize(selOptions);
+			let uniqueRandom = Array.from(new Set(selOptions.sort()));//console.log(new Set(selOptions), selOptions);
+
+			// replace td content with select component
+			$colntds.each((i,td) => {
+				const txt = $(td).text().replace(/\n */g, "").trim();
+				/* Bootstrap class styles and funtionality */
+				const $select = $('<select class="selectpicker form-control">').data("ans", txt);
+				const $options = UI_CreateSelectOptions(uniqueRandom);
+				
+				$select.append($options);
+				$select.click(function(){Clear()});
+
+				// replace td content with select
+				$(td).html($select);
+				
+			});
+
+		});
+		 
+
+		$('.selectpicker').popover
+		
+	}
+
+	function UI_CreateSelectOptions(ar) {
+		let list = "<option>select</option>";
+		for (let i = 0; i < ar.length; i++) {
+			const txt = ar[i];
+			list += '<option data-content="' + txt +'" title="' + txt + '">' + txt + '</option>';
+		}
+		
+		return list
+	}
+
+	/********************* HELPER FUNCTIONS *****************/
+	function Randomize(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			let j = Math.floor(Math.random() * (i + 1));
+			let temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
+	}
+
+	/********************* EVENT HANDLERS *****************/
+	function Clear() {
+		$("i").remove();
+	}
+	
+	function ResetAnswers() {
+		$("select").val('-1');
+		Clear();
+	}
+
+	function CheckAnswers(){
+		const $selects = $("select");
+		// using bootstrap icon css
+		Clear();
+
+		$selects.each(function(i,sel){
+			const $fb_icon_right = $('<i class="glyphicon glyphicon-ok"></i>');
+			const $fb_icon_wrong = $('<i class="glyphicon glyphicon-remove"></i>');
+			
+			const selected = $(sel).find(":selected").text();
+			const ans = $(sel).data("ans");
+			
+			if(selected == ans) {
+				$(sel).after($fb_icon_right);
+			}else{
+				$(sel).after($fb_icon_wrong);
+			}
+		});
+	}
+});
+
+
